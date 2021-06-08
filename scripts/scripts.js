ab8a19bc644fb879f1939b6c5efbc577b8b3638a
MykytaLiubshin <enotich13@gmail.com>
2021-06-08 21:34:30 +0300

    change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,26 @@
-const showImage = (link) => {};
+const imageLoader = () => {
+    const imageLoader = document.createElement('div')
+    imageLoader.innerHTML = `<div class="spinner-border text-warning" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>`
+  return imageLoader
+}
+
+const showImage = (link) => {
+    const cont = document.createElement('div')
+    cont.className = 'art'
+    cont.inner = imageLoader();
+    const image = document.createElement('img')
+    image.src = link;
+    image.className = "art";
+    image.onload = () => {cont.inner = image};
+    return cont
+};
+
+const AllImages = (classname = "artGallery") => {
+    element = document.querySelector(".artGallery");
+}
+// window.onload= () => {
+//     document.querySelector('.art1').innerHTML = showImage('./../static/art/1.jpg').innerHTML
+// console.log(document.getElementsByClassName('art1').innerHTML)
+// }
